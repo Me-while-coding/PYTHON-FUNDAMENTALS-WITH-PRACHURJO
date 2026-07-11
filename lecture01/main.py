8a814c9529cd216cb34489c52b8860c4d26acdb4
Me-while-coding <prachurjoofficial@gmail.com>
2026-07-11 14:12:02 +0530

    added lecture 1 content

---
+++ lecture01/main.py
@@ -0,0 +1,12 @@
+print("Hello guys welcome to this fundamental python course") #this is a print statement used to print anything in python.
+#this is the first video in this series
+
+"""
+Hello guys i am prachurjo
+from LPU and i am teaching 
+python to you guys
+hope u guys like it and enjoy learning!!!!!!!!!!!!!!!
+"""
+#Comments are, u can see guess by the name here , they are simply comments on your code
+#which we write to understand the code better and its a good practice by developers as
+#in the long run, it helps everyone understand the purpose and logic of complex stuff!
---
+++ lecture01/notes1.md
@@ -0,0 +1,18 @@
+What is Programming?
+
+Porgramming is the process of writing a set of instructions for a computer to follow and carry
+out a particular task.
+
+Python is one of the many programming languages.
+
+Invented by Guido Van Rossum in the late 1980s , Python became very popular due to it's high
+readability and syntax simplicity and wide range of applications.
+
+Its used in :
+1.Web developement
+2.Software developement.
+3.Machine Learning and Data Science
+4.Web scrapping
+5.Cybersecurity
+
+It's a high level language and follows object oriented programming.
