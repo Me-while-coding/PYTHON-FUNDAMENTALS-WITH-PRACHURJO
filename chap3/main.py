10710c95e0a9f344d132aca9c8d48892b1ef7ad1
Me-while-coding <prachurjoofficial@gmail.com>
2026-07-18 20:02:47 +0530

    renaming to chapters

---
+++ chap3/main.py
@@ -0,0 +1,68 @@
+#Datatypes
+
+# num = 3 # integer
+# print(num)
+# num = 3.45 #float
+# print(num)
+# num = 1 + 2j # complex numbers
+# print(num)
+
+# name = "Prachurjo"
+# college = "LPU"
+# book = "The Silent Patient"
+# item = 'chair'
+# print(name,college,book,item)
+
+list_1 = [1,2,"python",4.5,[2,3]] #list
+# for i in list_1:
+#     print(i)
+
+fruits = {"Apple","cherry","Mangoes"}
+# for i in fruits:
+#     print(i)
+
+# address= {"city":"Alipurduar","state":"West-Bengal","pin":736123}
+# print(address["pin"])
+
+# print(list_1[0])
+# print(list_1[1])
+# print(list_1[2])
+# print(list_1[3])
+# print(list_1[4])
+
+# tuple = (1,2,3)
+# #immutable : jisko aap modify nhi kr sakte ho
+# # tuple[0] = 4
+# # print(tuple[0])
+
+# #mutable : jismein aap values modify kr sakte ho and values add or delete bhi kr sakte ho
+# list_1[0] = "new_value"
+# print(list_1[0])
+# list_1.append(456)
+# for i in list_1:
+#     print(i)
+
+
+# str1 = 'abracadabra'
+# print(str1[0])
+
+# pass_status = True #boolean
+# pass_status = False #boolean
+
+
+a = 5
+print(float(a))
+b = float(a)
+b = str(a)
+print(b)
+print(type(a))
+print(type(b))
+c = True
+print(type(c))
+shopping = ['hotwheels car','Alchemist','T-shirt']
+print(type(shopping))
+
+a,b = 10,20
+print(a,b)
+a = b = c = 100
+print(a,b,c)
